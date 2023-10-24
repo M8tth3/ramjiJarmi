790d1621a1203d1a9b6b3df6ae397cd27c5f236c
M8tth3 <mw.mountaindragon@gmail.com>
2023-10-24 08:58:54 -0700

    bossupdate

--- souls/js/combat.js
+++ souls/js/combat.js
@@ -1,7 +1,7 @@
 
 let bossHealth = 1000;
-let timeRemaining = 5;
-
+let timeRemaining = 30;
+const bossAttacks = {'big swing':50, 'fast swing':30, 'sit on you':100};
 function updateHealth() {
     const healthBar = document.getElementById('currentHealth');
     healthBar.style.width = (bossHealth / 10) + '%';
@@ -17,24 +17,6 @@ function attack1() {
     console.log(bossHealth);
 }
 
-function attack2() {
-    let damage = 100;
-    bossHealth -= damage;
-    if (bossHealth < 0) bossHealth = 0;
-    updateHealth();
-    document.getElementById('attack2Message').textContent = 'You did ' + damage + ' damage! Boss health is now ' + bossHealth;
-    checkWinCondition();
-}
-
-function attack3() {
-    let damage = 25;
-    bossHealth -= damage;
-    if (bossHealth < 0) bossHealth = 0;
-    updateHealth();
-    document.getElementById('attack3Message').textContent = 'You did ' + damage + ' damage! Boss health is now ' + bossHealth;
-    checkWinCondition();
-}
-
 function checkWinCondition() {
     if (bossHealth <= 0) {
         clearInterval(timerInterval);
@@ -45,32 +27,17 @@ function checkWinCondition() {
     }
 }
 
-function updateTimerDisplay() {
-    document.getElementById('timer').textContent = 'Time remaining: ' + timeRemaining + ' seconds';
+function updatePlayerHealth(playerHealth)
+{
+    currentHealth = playerHealth
+    const healthBar = document.getElementById('stats');
+    const remainingHealth = "#".repeat(currentHealth) + ".".repeat(playerHealth-currentHealth);
+    healthBar.textContent = "HP: [" + remainingHealth + "]";
 }
 
-const timerInterval = setInterval(function() {
-    timeRemaining--;
-    updateTimerDisplay();
-
-    if (timeRemaining <= 0) {
-        clearInterval(timerInterval);
-        const bossImage = document.getElementById("bossImg");
-        bossImage.style.filter = "grayscale(100%)";
-        died = document.getElementById("died");
-        died.style.display = "block";
-        container = document.getElementById("youDied");
-        document.getElementById('buttons').style.display = 'none';
-        tryAgain = document.createElement("button");
-        tryAgain.classList.add("button");
-        tryAgain.innerText = "Try again"
-        tryAgain.setAttribute("onclick","window.location.href='/ramjiJarmi/souls/game.html'");
-        container.append(tryAgain);
-        // window.location.href = "/ramjiJarmi/souls/game.html";
-    }
-
-}, 1000);
-
+function damageCalc(){
+    return 70;
+}
 // Initialization
 window.onload = function() {
     updateTimerDisplay();
@@ -96,3 +63,59 @@ function dropItem() {
 
     //add item to the player's inventory here:
 }
+
+var numChat = 0;
+var tracker = 0;
+function newChatMessage(attacker, damageMessage)
+{
+    // const attacker = '> ChosenUndead~ ';
+    const message = document.createTextNode(`${attacker} ${damageMessage}`);
+    const chat = document.getElementById('chat');
+
+    const atkMessage = document.createElement('div');
+    atkMessage.setAttribute('id', `${numChat}`);
+    atkMessage.classList.add('newMessage');
+    atkMessage.appendChild(message);
+    chat.appendChild(atkMessage);
+    const newChat = document.getElementById(numChat);
+    newChat.style.padding = '6px';
+
+    numChat++;
+    const maxMessages = 13;
+    if(numChat > maxMessages)
+    {
+        const messageToRemove = document.getElementById(tracker);
+        messageToRemove.remove();
+        tracker++;
+    }
+}
+const bossImg = document.getElementById('bossImg');
+function bossAttack()
+{
+    //Takes the key names of the dictionary and makes it into a list
+    const attackKeys = Object.keys(bossAttacks);
+    //Picks a random index number based on the length of the array of key names
+    const randomIndex = Math.floor(Math.random()*attackKeys.length);
+    //Gets a random key name
+    const randomAttackKey = attackKeys[randomIndex];
+    //Puts the random key name into bossAttacks to get the damage value :)
+    var bossAttack = bossAttacks[randomAttackKey];
+    console.log(bossAttack);
+    newChatMessage('> Boss~ ', `did ${bossAttack} damage`)
+}
+window.addEventListener("keypress", function (keyPressed) {
+    if(keyPressed.key === 'f')
+    {
+        newChatMessage('> ChosenUndead~ ', ' inflicted 70 damage');  
+        bossImg.classList.add('bossDamage');
+
+        setTimeout(() => {
+            bossImg.classList.remove('bossDamage');
+        },300);
+        setTimeout(() => {
+            bossAttack();
+        },2000);
+    }})
+
+updatePlayerHealth(100);
+//Boss attacks
